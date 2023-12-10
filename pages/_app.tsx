9c30ae915b7c0eb19ed606f15d3277fdc9badd18
Mahmod <mahmodghnaj1@gmail.com>
2023-12-10 09:11:09 +0300

    sync state with url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    setReady(router.isReady);
+  }, [router.isReady]);
+
+  return <> {ready && <Component {...pageProps} />} </>;
 }
